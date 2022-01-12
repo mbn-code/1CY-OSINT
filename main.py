2970462d1f1e3a83ce2b1efd9dd58138cbd39004
CollinEdward <manorengaard@icloud.com>
2022-01-12 21:03:12 +0100

    More ip information gathering added to ip command.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
  # Main file made for running with python3.10
+from audioop import reverse
 import os
 import sys
 import platform
 from time import sleep
 import requests
+import random
 
 version = "BETA-V1.10"
 
@@ -13,9 +15,18 @@ def main_script(command: str) -> None:
             print(f"Version: {version}")
 
         case ["ip" | "ipinfo", ip]:
+            
+        
+
             api_req = requests.get(f"https://ipinfo.io/{ip}")
             with open(f"Ip_information{ip}.txt", "w") as ip_info:
                 ip_info.write(api_req.text)
+
+            os.system(f"whois {ip} > whois_{ip}.txt")
+
+
+            wpscan_ip = os.system(f"wpscan --url http://{ip}/ > wpscan_{ip}.txt")
+
 
         case ["help"]:
             print("ip, ipinfo - Get information on an ip")
@@ -27,7 +38,15 @@ def main_script(command: str) -> None:
 
 def main():
     while 1:
-        command = input(f"Hello, {platform.node()}: ")
+        list_greetings = [
+            "Hello, ", "Hola, ","Hej, ", "Marhaba, ",
+            "Grüß Gott, ", "Namaskar, ","Zdraveite, ",
+            "Nǐ hǎo, ","Dobar dan, ","Hallo, ","hyvää päivää, ",
+            "Bonjour, ","Guten tag, ","Namaste, ","Shalom, ","Salve, ",
+            "Konnichiwa, ","Ahn nyong ha se yo, ","Cześć, ","Olá, ","Sawasdee, ","Merhaba, "]
+
+        random_greeting = random.choice(list_greetings)
+        command = input(f"{random_greeting}{platform.node()}: ")
         main_script(command)
     
 if __name__ == "__main__":
@@ -38,7 +57,7 @@ if __name__ == "__main__":
     else:
         pass
 
-    words = "Welcome to 1CY-OSINT, the automated OSINT Toolkit for information gathering made easy\n"
+    words = "Welcome to 1CY-OSINT.\n"
     for char in words:
         sys.stdout.write(char)
         sys.stdout.flush()
